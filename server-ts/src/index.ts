28b05855e6d624a60c243dd85ecc60339cdd730e
ชนาธิป สุขพรสวรรค์ <first.sdz@gmail.com>
2023-01-03 00:35:50 +0700

    Import fixtures,config

--- server-ts/src/index.ts
+++ server-ts/src/index.ts
@@ -1,8 +1,13 @@
 import Koa from 'koa'
 
+import loadFixtures from './fixtures'
 import api from './api'
+import appConfig from './config'
 
 const app = new Koa()
+
 app
   .use(api.routes())
   .listen(8000)
+
+loadFixtures(appConfig.clearDataBeforeLodingFixture)
