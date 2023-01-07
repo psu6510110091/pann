9990419d8fbf0d21f29b4736d5e86293bdab0c4b
ชนาธิป สุขพรสวรรค์ <first.sdz@gmail.com>
2023-01-07 23:32:38 +0700

    Import koa-body,koa-cors

--- server-ts/src/index.ts
+++ server-ts/src/index.ts
@@ -1,4 +1,6 @@
 import Koa from 'koa'
+import { koaBody } from 'koa-body'
+import cors from '@koa/cors'
 
 import loadFixtures from './fixtures'
 import api from './api'
@@ -7,6 +9,9 @@ import appConfig from './config'
 const app = new Koa()
 
 app
+  .use(koaBody())
+  .use(cors())
+
   .use(api.routes())
   .listen(8000)
 
