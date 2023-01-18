762aad077dc10937a4583afca533bc5c72f8f677
ชนาธิป สุขพรสวรรค์ <first.sdz@gmail.com>
2023-01-19 01:51:53 +0700

    Create config/routes (only login)

---
+++ client-ts/src/config/routes.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes, } from 'react-router-dom';
+
+import Login from '../pages/login';
+//import UserResultList from '../pages/user-result-list';
+//import AnnouncementList from '../pages/announcement-list';
+//import { useAuth } from "react-oidc-context";
+//import { useAppCtx } from '../AppProvider';
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route index element={<Login />} />
+      <Route path="login" element={<Login/>} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
