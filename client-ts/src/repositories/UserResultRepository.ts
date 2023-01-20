8fe4e621388d2927a8788c9ecc8684d272c6772b
ชนาธิป สุขพรสวรรค์ <first.sdz@gmail.com>
2023-01-21 00:10:51 +0700

    Add All Method

--- client-ts/src/repositories/UserResultRepository.ts
+++ client-ts/src/repositories/UserResultRepository.ts
@@ -17,4 +17,44 @@ export class UserResultRepository implements IRepository<UserResult> {
 
     return resp.data
   }
+  
+  async get(id: string|number): Promise<UserResult | null> {
+    const resp = await ax.get<UserResult>(`${this.urlPrefix}/userResult/${id}`)
+
+    return resp.data
+  }
+
+  async create(entity: Partial<UserResult>): Promise<UserResult | null> {
+    const resp = await ax.post<UserResult>(`${this.urlPrefix}/userResult`, entity)
+
+    return resp.data
+  }
+
+  async update(entity: Partial<UserResult>): Promise<UserResult | null> {
+    const resp = await ax.put<UserResult>(`${this.urlPrefix}/userResult/${entity.id}`, entity)
+
+    return resp.data
+  }
+
+  async delete(id: string|number): Promise<void> {
+    await ax.delete<void>(`${this.urlPrefix}/userResult/${id}`)
+  }
+
+  async view(id: string|number): Promise<UserResult | null> {
+    const resp = await ax.get<UserResult>(`${this.urlPrefix}/userResult/view/${id}`)
+
+    return resp.data
+  }
+
+  async acknowledge(id: string|number): Promise<UserResult | null> {
+    const resp = await ax.get<UserResult>(`${this.urlPrefix}/userResult/acknowledge/${id}`)
+
+    return resp.data
+  }
+
+  async toggleIsPinned(id: string|number): Promise<UserResult | null> {
+    const resp = await ax.get<UserResult>(`${this.urlPrefix}/userResult/toggleIsPinned/${id}`)
+
+    return resp.data
+  }
 }
