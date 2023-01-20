b2c84e3ac6ae3e4e2abae2a8ec9c163ecec8ec6c
ชนาธิป สุขพรสวรรค์ <first.sdz@gmail.com>
2023-01-20 23:41:23 +0700

    Replace getAll in UserResultRepository

--- client-ts/src/repositories/UserResultRepository.ts
+++ client-ts/src/repositories/UserResultRepository.ts
@@ -1,66 +1,20 @@
-import UserResult from "../models/UserResult";
+import { ax } from '../config';
 import { IRepository } from "./IRepository";
+import UserResult from "../models/UserResult";
+import config from "../config";
+
+export interface UserResultFilter {
+  keyword?: string
+  isPinned?: boolean
+}
 
 export class UserResultRepository implements IRepository<UserResult> {
+  urlPrefix = config.remoteRepositoryUrlPrefix
 
-  async getAll(): Promise<UserResult[] | null> {
+  async getAll(filter: UserResultFilter): Promise<UserResult[] | null> {
+    const params = {...filter}
+    const resp = await ax.get<UserResult[]>(`${this.urlPrefix}/userResult`, { params })
 
-    return [
-      {
-        "id": 1,
-        "announcement": {
-            "id": 1,
-            "topic": "240-124 Midterm 1/2566",
-            "description": "คะแนนกลางภาควิชา Web Dev",
-            "pubDateTime": new Date("2022-09-08T03:00:00.000Z")
-        },
-        "result": "24.5",
-        "resultType": 1,
-        "remark": "โดนหักคะแนนจากการเข้าสอบสาย",
-        "isPinned": false,
-        "viewDateTime": null,
-        "ackDateTime": null,
-        "updateDateTime": new Date("2022-09-07T02:12:31.000Z"),
-        "expireDateTime": null,
-        "userCode": "6210110010"
-      },
-      {
-        "id": 2,
-        "announcement": {
-            "id": 1,
-            "topic": "240-124 Midterm 1/2566",
-            "description": "คะแนนกลางภาควิชา Web Dev",
-            "pubDateTime": new Date("2022-09-08T03:00:00.000Z")
-        },
-        "result": "30.1",
-        "resultType": 1,
-        "remark": "",
-        "isPinned": false,
-        "viewDateTime": null,
-        "ackDateTime": null,
-        "updateDateTime": new Date("2022-09-07T04:12:00.000Z"),
-        "expireDateTime": null,
-        "userCode": "6210110227"
-      },
-      {
-        id: 3,
-        announcement: {
-            id: 2,
-            topic: "ทุนเรียนดีประจำปี 2566",
-            description: "test",
-            remarkIfPositive: "ขอแสดงความยินดีกับผุ้ได้รับทุนเรียนดีทุกคนด้วย",
-            pubDateTime: new Date("2022-09-09T08:00:00.000Z")
-        },
-        result: "ได้รับทุน",
-        resultType: 1,
-        remark: "",
-        isPinned: false,
-        viewDateTime: null,
-        ackDateTime: null,
-        updateDateTime: new Date("2022-09-08T03:12:30.000Z"),
-        expireDateTime: null,
-        userCode: "6210110227"
-      }
-  ]
-}
+    return resp.data
+  }
 }
