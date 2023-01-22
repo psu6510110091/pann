d06592df17645be53c1d67c8a61c1230e3f9433a
ชนาธิป สุขพรสวรรค์ <first.sdz@gmail.com>
2023-01-22 15:41:12 +0700

    Add Login CSS

--- client-ts/src/pages/login.tsx
+++ client-ts/src/pages/login.tsx
@@ -1,67 +1,76 @@
 import { useEffect } from 'react';
-import { Button } from '@mui/material';
-import { Box } from '@mui/system';
-import { Login as LoginIcon } from '@mui/icons-material';
 import { useAuth } from "react-oidc-context";
 import { Navigate, useLocation } from 'react-router-dom';
 import { useAppCtx } from '../AppProvider';
+import './Login.css';
 
 function Login() {
-  const { userInfo, action } = useAppCtx()
-  const auth = useAuth()
-  const location = useLocation()
+  const { userInfo, action } = useAppCtx();
+  const auth = useAuth();
+  const location = useLocation();
 
   console.log('rendering..... login', auth.user)
   useEffect(() => {
-    if(auth.isAuthenticated) {
+    if (auth.isAuthenticated) {
       setTimeout(() => {
-          action.setUserInfo({
-              ready: true,
-              username: auth.user?.profile.preferred_username,
-              displayName: auth.user?.profile.given_name + ' ' + auth.user?.profile.family_name
-          })
-      },1000)
-  }
-},[auth, userInfo.ready , action])
+        action.setUserInfo({
+          ready: true,
+          username: auth.user?.profile.preferred_username,
+          displayName: auth.user?.profile.given_name + ' ' + auth.user?.profile.family_name
+        })
+      }, 1000)
+    }
+  }, [auth, userInfo.ready])
 
   switch (auth.activeNavigator) {
     case "signinSilent":
-      return <div>Signing you in...</div>
-  case "signinRedirect":
-      return <div>Signing you out...</div>
+      return <div>Signing you in...</div>;
+    case "signoutRedirect":
+      return <div>Signing you out...</div>;
   }
 
-  if (auth.isLoading){
-      return <div>Loading...</div>;
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
   }
 
   if (auth.error) {
-     return <div>Oops... {auth.error.message}</div>;
+    return <div>Oops... {auth.error.message}</div>;
   }
 
   if (auth.isAuthenticated) {
-      if (userInfo.ready) {
-        const backTo = location.state?.backTo || '/home'
-        if(action.isStaff()){
-            return(
-                <Navigate to = '/announcement' replace />
-            )    
-        }
-        return (
-            <Navigate to={backTo} replace />
-        )
+    if (userInfo.ready) {
+      const backTo = location.state?.backTo || '/home'
+      if(action.isStaff()){
+        return(
+            <Navigate to = '/announcement' replace />
+        )    
+    }
+      return (
+        <Navigate to={backTo} replace />
+      );
     } else {
-        return <div>Waiting for whoami </div>
+      return <div>Waiting for whoami</div>;
     }
   }
 
   return (
-    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: 500 }}>
-      <Button variant='contained' sx={{ fontSize: 'large' }} onClick={() => void auth.signinRedirect()}>
-        <LoginIcon sx={{ mr: 1 }} />
-        Log in
-      </Button>
-    </Box>
+    <div className="login-container">
+      <div className="logo-button-container">
+        <div className="content-container">
+          <div className="logo-container">
+            <img src={require('../Image/Logo-PSU-EH-01.png')} alt="Logo" className="logo-img" />
+          </div>
+          <div className="text-content-wrapper">
+            <div className="text-content">Welcome to PSU Announcement!</div>
+          </div>
+        </div>
+        <div className="button-container">
+          <button className="login-button" onClick={() => void auth.signinRedirect()}>
+            Log in
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
